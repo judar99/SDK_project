02c5c17929f7c529c28acfe903d4b97d2f9843ee
judar99 <judaar.99@gmail.com>
2023-03-02 19:00:37 -0500

    First Commit

--- bin/cdk-first-app.ts
+++ bin/cdk-first-app.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
 import { CdkFirstAppStack } from '../lib/cdk-first-app-stack';
 
 const app = new cdk.App();
--- lib/cdk-first-app-stack.ts
+++ lib/cdk-first-app-stack.ts
@@ -1,9 +1,22 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as apigw from "@aws-cdk/aws-apigateway";
+
+
+import { Lambda } from 'aws-cdk-lib/aws-ses-actions';
+
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkFirstAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkFirstAppQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
   }
 }
