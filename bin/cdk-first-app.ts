12b5d8d8d83ea286bce5f1993c9dc2579ba5a985
judar99 <judaar.99@gmail.com>
2023-03-03 10:36:38 -0500

    lambda implementation

--- bin/cdk-first-app.ts
+++ bin/cdk-first-app.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { CdkFirstAppStack } from '../lib/cdk-first-app-stack';
 
 const app = new cdk.App();
--- lib/cdk-first-app-stack.ts
+++ lib/cdk-first-app-stack.ts
@@ -2,21 +2,38 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as dynamodb from "@aws-cdk/aws-dynamodb";
 import * as apigw from "@aws-cdk/aws-apigateway";
+const path = require('path');
 
 
-import { Lambda } from 'aws-cdk-lib/aws-ses-actions';
-
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkFirstAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //Dynamodb
+    const greetingsTable = new dynamodb.Table(this,"GreetingTable",{
+      partitionKey: {name: "id", type: dynamodb.AttributeType.STRING},
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkFirstAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //Lambda
+    const saveHellloFuntion = new lambda.Function(this,"SaveHelloFuntion",{
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'handler.saveHello',
+      code: lambda.Code.fromAsset(path.resolve(__dirname,'lambda')),
+      environment: {
+        GREETINGS_TABLE: greetingsTable.tableName,
+      },
+    });
+    //permiso para lambda
+    greetingsTable.grantWriteData(saveHellloFuntion);
+
+    //APIgatway
+    const helloAPI =new apigw.RestApi(this,"helloApi");
+
+    helloAPI.root
+      .resourceForPath("helo")
+      .addMethod("POST", new apigw.LambdaIntegration(saveHellloFuntion))
+
+
   }
 }
